8ffd29be92a540083f2314f77650c33c27b886f6
Tanutchok <chokolate40@gmail.com>
2024-11-13 18:16:55 +0700

    add new files

---
+++ js/owl_script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    merge: true,
+    nav: true,
+    center: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 1800,
+    loop: true,
+    margin: 10,
+    video: true,
+    lazyload: true
+});
+
